dfb91777cc5f90d23074b1dcf92c995483f196e4
ninotbs <nino_tbs@mac.com>
2023-09-24 21:11:41 +0200

    mongo db

---
+++ db/mongo/init/02_createindex.js
@@ -0,0 +1,11 @@
+print('Erzeuge GeoJson Punkte...')
+let i = 0
+db.geoname.find().forEach(data => {
+  i++
+  db.geoname.update( { _id: data._id }, {
+    $set: { location: { type: 'Point',
+      coordinates: [parseFloat(data.longitude), parseFloat(data.latitude)] } }
+  })
+})
+print(i + ' Punkte erzeugt, erstelle 2d-Index...')
+db.geoname.createIndex( { location: '2dsphere' } )
